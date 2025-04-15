eb3e470a53c0b001b3875a9182d8153ab4b9e58e
Natanael Romero de Assunção <nathanpadev@gmail.com>
2025-04-15 14:23:02 -0300

    alterações

---
+++ sistema-smart-tv/src/Comentarios.java
@@ -0,0 +1,22 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário de uma unica linha
+
+        /*
+         * Olá, eu sou um comentário de múltiplas linhas
+         * que posso ser mais detalhado
+         * quando necessário.
+         */
+
+    }
+ /**
+  * Estas duas estrelinhas acima
+  é para identificar que você
+  pretende elaborar um comentário
+  a nivel de documentação.
+  Que incrivel!!!
+    */
+
+    public void metodo(){}
+    
+}
